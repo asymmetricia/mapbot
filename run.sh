46e05f330a1b6d3a54efecda4da96a65d33296a5
Patrick Bogen <pdbogen@cernu.us>
2017-07-22 14:15:02 -0700

    verification token & quotes

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 /mapbot \
-  --db-host=$DB_HOST \
-  --db-user=$DB_USER \
-  --db-name=$DB_NAME \
-  --db-pass=$DB_PASS \
-  --slack-client-id=$CLIENT_ID \
-  --slack-client-secret=$CLIENT_SECRET \
-  --domain=$FQDN \
-  --port=$PORT
+  --db-host="$DB_HOST" \
+  --db-user="$DB_USER" \
+  --db-name="$DB_NAME" \
+  --db-pass="$DB_PASS" \
+  --slack-client-id="$CLIENT_ID" \
+  --slack-client-secret="$CLIENT_SECRET" \
+  --slack-verification-token="$VERIFICATION_TOKEN" \
+  --domain="$FQDN" \
+  --port="$PORT"
