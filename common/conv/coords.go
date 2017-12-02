9890d095fae32c9ff29e41aa8b7fa2cada446ed8
pdbogen <pdbogen@slack-corp.com>
2017-12-01 16:14:41 -0800

    Support RC-style (A1 B2) coords

--- common/conv/coords.go
+++ common/conv/coords.go
@@ -11,14 +11,23 @@ import (
 
 var xCoordRe = regexp.MustCompile(`^[a-zA-Z]+$`)
 var yCoordRe = regexp.MustCompile(`^[0-9]+$`)
+var coordRe = regexp.MustCompile(`^([a-zA-Z]+)([0-9]+)$`)
+
+func RCToPoint(rc string) (image.Point, error) {
+	if matches := coordRe.FindStringSubmatch(rc); matches != nil || len(matches) < 3 {
+		return CoordsToPoint(matches[1], matches[2])
+	} else {
+		return image.Point{}, errors.New("not an RC coordinate")
+	}
+}
 
 func CoordsToPoint(x, y string) (image.Point, error) {
 	if !xCoordRe.MatchString(x) {
-		return image.Point{}, errors.New("X coordinage must be a column letter")
+		return image.Point{}, errors.New("X coordinate must be a column letter")
 	}
 
 	if !yCoordRe.MatchString(y) {
-		return image.Point{}, errors.New("Y coordinage must be a number")
+		return image.Point{}, errors.New("Y coordinate must be a number")
 	}
 
 	accumX := 0
