c7055e52d910d9f451c15297299acd2802a185cd
Patrick Bogen <pdbogen@cernu.us>
2020-03-14 11:25:06 -0700

    websockety

--- js/main.go
+++ js/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/gopherjs/websocket"
 	"honnef.co/go/js/dom"
 	"net/url"
@@ -26,10 +27,14 @@ func MapUrl(uri *url.URL) string {
 }
 
 func WsUrl(uri *url.URL) string {
+	var proto = "ws"
+	if dom.GetWindow().Location().Protocol == "https" {
+		proto = "wss"
+	}
 	sessionId := uri.Query().Get("id")
 
 	return (&url.URL{
-		Scheme: "wss",
+		Scheme: proto,
 		Host:   uri.Host,
 		Path:   strings.TrimRight(uri.Path, "/") + "/ws",
 		RawQuery: url.Values{
@@ -48,39 +53,54 @@ func main() {
 	dom.GetWindow().Document().AddEventListener("DOMContentLoaded", false, ready)
 }
 
+var ImageElement *dom.HTMLImageElement
+
+func updateMap(payload map[string]interface{}) {
+	ImageElement.Src = MapUrl(uri)
+}
+
+func connectWebsocket() {
+	conn, err := websocket.Dial(WsUrl(uri))
+	if err != nil {
+		print("websocket to ", WsUrl(uri), " error: ", err.Error())
+		return
+	}
+	defer conn.Close()
+
+	decoder := json.NewDecoder(conn)
+	var message map[string]interface{}
+	for {
+		if err := decoder.Decode(&message); err != nil {
+			print("decoding message: ", err.Error())
+			return
+		}
+		switch cmd := message["cmd"]; cmd {
+		case "update":
+			updateMap(message)
+		default:
+			print("ignoring message: ", cmd)
+		}
+	}
+}
+
 func ready(dom.Event) {
+	imgElem := dom.GetWindow().Document().GetElementByID("mapimage")
+
+	if imgElem == nil {
+		print("troubling, no element with id `mapimage` could be found")
+		return
+	}
+
+	var ok bool
+	ImageElement, ok = imgElem.(*dom.HTMLImageElement)
+	if !ok {
+		print("`mapimage` was found, but was ", imgElem.TagName(), ", not an image")
+		return
+	}
+
 	go func() {
 		for {
-			imgElem := dom.GetWindow().Document().GetElementByID("mapimage")
-
-			if imgElem == nil {
-				print("troubling, no `mapimage` could be found")
-				return
-			}
-
-			img, ok := imgElem.(*dom.HTMLImageElement)
-			if !ok {
-				print("`mapimage` was found, but was ", img.TagName(), ", not an image")
-				return
-			}
-
-			img.Src = MapUrl(uri)
-
-			conn, err := websocket.Dial(WsUrl(uri))
-			if err == nil {
-				buf := make([]byte, 1024)
-				for {
-					_, err := conn.Read(buf)
-					if err != nil {
-						break
-					}
-					img.Src = MapUrl(uri)
-				}
-			} else {
-				print("websocket to ", WsUrl(uri), " error: ", err.Error())
-			}
-
-			time.Sleep(time.Second)
+			connectWebsocket()
 		}
 	}()
 }
